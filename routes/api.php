e1b572139c4e6bf5a34150a241c470216a3e02e5
NdeyeMbombeCisse <ndeyecisse188@gmail.com>
2024-08-06 16:51:09 +0000

    middleware

--- routes/api.php
+++ routes/api.php
@@ -12,15 +12,19 @@ use App\Http\Controllers\EvaluationController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
 Route::post('login',[AuthController::class,'login']);
 Route::get('logout',[AuthController::class,'logout']);
 
-Route::apiResource('etudiants',EtudiantController::class);
-Route::get('restore/{id}',[EtudiantController::class,'restore']);
-Route::get('forceDelete/{id}',[EtudiantController::class,'forceDelete']);
-Route::get('trashed',[EtudiantController::class,'trashed']);
 
-Route::apiResource('evaluations',EvaluationController::class);
-
-
+Route::middleware('auth')->group(
+    function(){
+        Route::apiResource('etudiants',EtudiantController::class);
+        Route::get('restore/{id}',[EtudiantController::class,'restore']);
+        Route::get('forceDelete/{id}',[EtudiantController::class,'forceDelete']);
+        Route::get('trashed',[EtudiantController::class,'trashed']);
+        Route::apiResource('evaluations',EvaluationController::class);
+    }
+);
 
