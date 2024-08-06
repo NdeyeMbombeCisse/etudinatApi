6d5e9bc17f2bee06bd7e48e498b22afa71b893a6
NdeyeMbombeCisse <ndeyecisse188@gmail.com>
2024-08-06 12:36:05 +0000

    gestion d'affichage ds etudiants

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController; 
+use App\Http\Controllers\EtudiantController; 
+
 
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 Route::post('login',[AuthController::class,'login']);
+Route::apiResource('etudiants',EtudiantController::class);
