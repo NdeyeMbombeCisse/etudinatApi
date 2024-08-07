81e2e5a0af8b51a5c14d74e945e1e1106180c28d
NdeyeMbombeCisse <ndeyecisse188@gmail.com>
2024-08-07 12:47:52 +0000

    documentation

---
+++ app/Http/Controllers/Annotations/EtudiantAnnotationController.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace App\Http\Controllers\Annotations ;
+
+/**
+ * @OA\Security(
+ *     security={
+ *         "BearerAuth": {}
+ *     }),
+
+ * @OA\SecurityScheme(
+ *     securityScheme="BearerAuth",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT"),
+
+ * @OA\Info(
+ *     title="Your API Title",
+ *     description="Your API Description",
+ *     version="1.0.0"),
+
+ * @OA\Consumes({
+ *     "multipart/form-data"
+ * }),
+
+ *
+
+ * @OA\GET(
+ *     path="/api/trashed",
+ *     summary="afficher etudiants sup",
+ *     description="",
+ *         security={
+ *    {       "BearerAuth": {}}
+ *         },
+ * @OA\Response(response="200", description="OK"),
+ * @OA\Response(response="404", description="Not Found"),
+ * @OA\Response(response="500", description="Internal Server Error"),
+ *     @OA\Parameter(in="header", name="User-Agent", required=false, @OA\Schema(type="string")
+ * ),
+ *     tags={"etudiant"},
+*),
+
+
+ * @OA\GET(
+ *     path="/api/forceDelete/{id}",
+ *     summary="forcedelete",
+ *     description="",
+ *         security={
+ *    {       "BearerAuth": {}}
+ *         },
+ * @OA\Response(response="200", description="OK"),
+ * @OA\Response(response="404", description="Not Found"),
+ * @OA\Response(response="500", description="Internal Server Error"),
+ *     @OA\Parameter(in="path", name="id", required=false, @OA\Schema(type="string")
+ * ),
+ *     @OA\Parameter(in="header", name="User-Agent", required=false, @OA\Schema(type="string")
+ * ),
+ *     tags={"etudiant"},
+*),
+
+
+ * @OA\GET(
+ *     path="/api/restore/{id}",
+ *     summary="Restaurer etudiant",
+ *     description="",
+ *         security={
+ *    {       "BearerAuth": {}}
+ *         },
+ * @OA\Response(response="200", description="OK"),
+ * @OA\Response(response="404", description="Not Found"),
+ * @OA\Response(response="500", description="Internal Server Error"),
+ *     @OA\Parameter(in="header", name="User-Agent", required=false, @OA\Schema(type="string")
+ * ),
+ *     tags={"etudiant"},
+*),
+
+
+ * @OA\DELETE(
+ *     path="/api/etudiants/{id}",
+ *     summary="supprimer etudiant",
+ *     description="",
+ *         security={
+ *    {       "BearerAuth": {}}
+ *         },
+ * @OA\Response(response="204", description="Deleted successfully"),
+ * @OA\Response(response="401", description="Unauthorized"),
+ * @OA\Response(response="403", description="Forbidden"),
+ * @OA\Response(response="404", description="Not Found"),
+ *     @OA\Parameter(in="path", name="id", required=false, @OA\Schema(type="string")
+ * ),
+ *     @OA\Parameter(in="header", name="User-Agent", required=false, @OA\Schema(type="string")
+ * ),
+ *     tags={"etudiant"},
+*),
+
+
+ * @OA\PUT(
+ *     path="/api/etudiants/{id}",
+ *     summary="Modifier etudiant",
+ *     description="",
+ *         security={
+ *    {       "BearerAuth": {}}
+ *         },
+ * @OA\Response(response="200", description="OK"),
+ * @OA\Response(response="404", description="Not Found"),
+ * @OA\Response(response="500", description="Internal Server Error"),
+ *     @OA\Parameter(in="path", name="id", required=false, @OA\Schema(type="string")
+ * ),
+ *     @OA\Parameter(in="header", name="User-Agent", required=false, @OA\Schema(type="string")
+ * ),
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\MediaType(
+ *             mediaType="application/x-www-form-urlencoded",
+ *             @OA\Schema(
+ *                 type="object",
+ *                 properties={
+ *                 },
+ *             ),
+ *         ),
+ *     ),
+ *     tags={"etudiant"},
+*),
+
+
+ * @OA\POST(
+ *     path="/api/etudiants",
+ *     summary="Ajout Etudiants",
+ *     description="",
+ *         security={
+ *    {       "BearerAuth": {}}
+ *         },
+ * @OA\Response(response="201", description="Created successfully"),
+ * @OA\Response(response="400", description="Bad Request"),
+ * @OA\Response(response="401", description="Unauthorized"),
+ * @OA\Response(response="403", description="Forbidden"),
+ *     @OA\Parameter(in="header", name="User-Agent", required=false, @OA\Schema(type="string")
+ * ),
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\MediaType(
+ *             mediaType="multipart/form-data",
+ *             @OA\Schema(
+ *                 type="object",
+ *                 properties={
+ *                     @OA\Property(property="prenom", type="string"),
+ *                     @OA\Property(property="nom", type="string"),
+ *                     @OA\Property(property="adresse", type="string"),
+ *                     @OA\Property(property="email", type="string"),
+ *                     @OA\Property(property="date_naissance", type="string"),
+ *                     @OA\Property(property="photo", type="string", format="binary"),
+ *                     @OA\Property(property="matricule", type="string"),
+ *                     @OA\Property(property="password", type="string"),
+ *                 },
+ *             ),
+ *         ),
+ *     ),
+ *     tags={"etudiant"},
+*),
+
+
+ * @OA\GET(
+ *     path="/api/etudiants",
+ *     summary="listetudiant",
+ *     description="",
+ *         security={
+ *    {       "BearerAuth": {}}
+ *         },
+ * @OA\Response(response="200", description="OK"),
+ * @OA\Response(response="404", description="Not Found"),
+ * @OA\Response(response="500", description="Internal Server Error"),
+ *     @OA\Parameter(in="header", name="User-Agent", required=false, @OA\Schema(type="string")
+ * ),
+ *     tags={"etudiant"},
+*),
+
+
+*/
+
+ class EtudiantAnnotationController {}
---
+++ app/Http/Controllers/Annotations/EvaluationAnnotationController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers\Annotations ;
+
+/**
+ * @OA\Security(
+ *     security={
+ *         "BearerAuth": {}
+ *     }),
+
+ * @OA\SecurityScheme(
+ *     securityScheme="BearerAuth",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT"),
+
+ * @OA\Info(
+ *     title="Your API Title",
+ *     description="Your API Description",
+ *     version="1.0.0"),
+
+ * @OA\Consumes({
+ *     "multipart/form-data"
+ * }),
+
+ *
+
+ * @OA\PUT(
+ *     path="/api/evaluations/{id}",
+ *     summary="modifier evaluation",
+ *     description="",
+ *         security={
+ *    {       "BearerAuth": {}}
+ *         },
+ * @OA\Response(response="200", description="OK"),
+ * @OA\Response(response="404", description="Not Found"),
+ * @OA\Response(response="500", description="Internal Server Error"),
+ *     @OA\Parameter(in="path", name="id", required=false, @OA\Schema(type="string")
+ * ),
+ *     @OA\Parameter(in="header", name="User-Agent", required=false, @OA\Schema(type="string")
+ * ),
+ *     tags={"Evaluation"},
+*),
+
+
+ * @OA\DELETE(
+ *     path="/api/evaluations/{id}",
+ *     summary="sup Evaluation",
+ *     description="",
+ *         security={
+ *    {       "BearerAuth": {}}
+ *         },
+ * @OA\Response(response="204", description="Deleted successfully"),
+ * @OA\Response(response="401", description="Unauthorized"),
+ * @OA\Response(response="403", description="Forbidden"),
+ * @OA\Response(response="404", description="Not Found"),
+ *     @OA\Parameter(in="path", name="id", required=false, @OA\Schema(type="string")
+ * ),
+ *     @OA\Parameter(in="header", name="User-Agent", required=false, @OA\Schema(type="string")
+ * ),
+ *     tags={"Evaluation"},
+*),
+
+
+ * @OA\POST(
+ *     path="/api/evaluations",
+ *     summary="ajoutEvaluation",
+ *     description="",
+ *         security={
+ *    {       "BearerAuth": {}}
+ *         },
+ * @OA\Response(response="201", description="Created successfully"),
+ * @OA\Response(response="400", description="Bad Request"),
+ * @OA\Response(response="401", description="Unauthorized"),
+ * @OA\Response(response="403", description="Forbidden"),
+ *     @OA\Parameter(in="header", name="User-Agent", required=false, @OA\Schema(type="string")
+ * ),
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\MediaType(
+ *             mediaType="multipart/form-data",
+ *             @OA\Schema(
+ *                 type="object",
+ *                 properties={
+ *                     @OA\Property(property="valeur", type="string"),
+ *                     @OA\Property(property="user_id", type="string"),
+ *                     @OA\Property(property="matiere_id", type="string"),
+ *                     @OA\Property(property="date", type="string"),
+ *                 },
+ *             ),
+ *         ),
+ *     ),
+ *     tags={"Evaluation"},
+*),
+
+
+ * @OA\GET(
+ *     path="/api/evaluations",
+ *     summary="afficheEvaluation",
+ *     description="",
+ *         security={
+ *    {       "BearerAuth": {}}
+ *         },
+ * @OA\Response(response="200", description="OK"),
+ * @OA\Response(response="404", description="Not Found"),
+ * @OA\Response(response="500", description="Internal Server Error"),
+ *     @OA\Parameter(in="header", name="User-Agent", required=false, @OA\Schema(type="string")
+ * ),
+ *     tags={"Evaluation"},
+*),
+
+
+*/
+
+ class EvaluationAnnotationController {}
